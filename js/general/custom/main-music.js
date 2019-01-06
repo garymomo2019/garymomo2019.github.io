6ddf87a5790d981dcec19157b191025db9abd419
garymomo2019 <garymomo2019@gmail.com>
2019-01-07 00:54:06 +0800

    2019/01/07 暫時修正分頁問題與例行更新

--- js/general/custom/main-music.js
+++ js/general/custom/main-music.js
@@ -17,8 +17,8 @@
 
 		$(".mainContain").css("height","auto");
 		if($(window).height() > $(".mainContain").css("height").substring(0, $(".mainContain").css("height").indexOf("px"))) {
-			setheight = $(window).height();
-			$(".mainContain").css("height", setheight);
+//			setheight = $(window).height();
+//			$(".mainContain").css("height", setheight);
 		}
 
 		if (width != ori_width) {
